2f72e1ab292c1f9a58289ff943fef5a5bec4d2a6
JonhClein <152296393+JonhClein@users.noreply.github.com>
2024-03-25 18:56:40 +0000

    casi listo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import "./App.css";
 import CreateTask from "./components/CreateTask";
 import ListTask from "./components/ListTasks";
 import toast , {Toaster} from "react-hot-toast";
-
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 const App: React.FC = () => {
   const [tasks , setTasks] = useState([]);
@@ -16,13 +17,15 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <div className="bg-slate-100 w-screen h-screen  flex flex-col items-center pt-3 gap-16 pt-32 ">
+      <DndProvider backend={HTML5Backend}>
+          <div className="bg-slate-100 w-screen h-screen  flex flex-col items-center pt-3 gap-16 pt-32 ">
         <Toaster/>
         <h1 className="text-center text-3xl">Todo List</h1>
         <CreateTask tasks={tasks} setTasks={setTasks} /> 
         <ListTask  tasks={tasks} setTasks={setTasks} />
         
     </div>
+      </DndProvider>
   )
 };
 
--- src/components/ListTasks.tsx
+++ src/components/ListTasks.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { MdOutlineDeleteOutline } from "react-icons/md";
+import toast from "react-hot-toast"; 
+import { useDrag, useDrop } from "react-dnd";
 const ListTask = ({ tasks , setTasks}) =>{
     const [todos , setTodos] = useState([]);
     const [inProgress , setInProgress] = useState([]);
@@ -35,6 +37,32 @@ const ListTask = ({ tasks , setTasks}) =>{
 export default ListTask;
 
 const Section = ({status , tasks  , setTasks , todos, inProgress , closed}) =>{
+    const [{isOver} , drop] = useDrop(()=>({
+        accept : "task" , 
+        drop : (item) =>addItemToSection(item.id),
+        collect : (monitor) => ({
+            isOver : !!monitor.isOver() , 
+        })
+    }))
+
+  const addItemToSection = (id) =>{
+    console.log("droped" , id);
+    setTasks((prev) =>{
+        const mTask = prev.map((t) =>{
+            if(t.id === id){
+                return {...t , status:status}
+            }
+
+            return t ;
+        })
+
+        localStorage.setItem("tasks" , JSON.stringify(mTask));
+        toast.success("Se Movio")
+        return mTask;
+    })
+  }
+
+
     let text = "Todo";
     let bg = "bg-slate-500";
     let tasksToMap = todos;
@@ -54,7 +82,9 @@ const Section = ({status , tasks  , setTasks , todos, inProgress , closed}) =>{
 
         
     return (
-        <div className={`w-64 h-64`}>
+        <div 
+         ref={drop}
+        className={`w-64 h-64 rounded-md p-2 ${isOver ? "bg-slate-200" : ""}`}>
            <Header text={text} bg={bg} count={tasksToMap.length} /> 
            {tasksToMap.length > 0 && tasksToMap.map(task => (
             <Tasks key={task.id}
@@ -78,14 +108,29 @@ const Header = ({text , bg , count}) =>{
 
 
 const Tasks = ({task , tasks , setTasks}) =>{
+    const [{isDragging} , drag] = useDrag(()=>({
+        type : "task" , 
+        item: {id : task.id} ,
+        collect : (monitor) => ({
+            isDragging : !!monitor.isDragging() 
+        })
+    }))
 
+    console.log(isDragging)
     const handleRemove = (id)=>{
         console.log(id);
-        const fTask = tasks.filter(t => t.id!== id)
+        const fTask = tasks.filter(t => t.id!== id);
+        localStorage.setItem("tasks" , JSON.stringify(fTask))
+        setTasks(fTask);
+        toast("Task Removed")
     }
     return (
-       <div className="relative p-4 mt-8 shadow-md rounded-md cursor-grab">
-            <p>{task.name}</p>
+       <div 
+        ref={drag}
+       className="
+        ${isDragging ? 'opacity-25' : 'opacity-100'}
+       relative p-4 mt-8 shadow-md rounded-md cursor-grab">
+            <p>{task?.name}</p>
             <MdOutlineDeleteOutline  className=" absolute button-1 right-1 text-slate-400" 
              onClick={()=>handleRemove(task.id)}
             />
